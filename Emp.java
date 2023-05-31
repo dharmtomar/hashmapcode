1bf4ef23e4a8487cee38ad74ca2e1747f39ed6a3
dharmtomar <dtdtomar2@gmail.com>
2023-05-31 16:49:52 +0530

    HashImplementation.java

---
+++ Emp.java
@@ -0,0 +1,15 @@
+package collectionFramwork;
+
+public class Emp {
+
+	String name;
+	String course;
+	int id;
+	int age;
+	public Emp(String name,String course,int id,int age) {
+		this.name=name;
+		this.course=course;
+		this.id=id;
+		this.age=age;
+	}
+}
